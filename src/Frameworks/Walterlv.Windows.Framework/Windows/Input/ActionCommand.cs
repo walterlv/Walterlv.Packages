3ac7a445cce61f70cfb286603dabfd5d78797a1b
walterlv <xblero@gmail.com>
2019-08-05 12:29:14 +0800

    Add action command.

---
+++ src/Frameworks/Walterlv.Windows.Framework/Windows/Input/ActionCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Walterlv.Windows.Input
+{
+    /// <summary>
+    /// 为普通的动作提供 <see cref="ICommand"/> 的实现。
+    /// </summary>
+    public class ActionCommand : ICommand
+    {
+        /// <summary>
+        /// 创建 <see cref="ActionCommand"/> 的新实例，当 <see cref="ICommand"/> 被执行时，将调用参数传入的动作。
+        /// </summary>
+        public ActionCommand(Action action)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+
+        /// <summary>
+        /// 此 <see cref="ActionCommand"/> 中用于执行的任务本身。
+        /// </summary>
+        private readonly Action _action;
+
+        /// <summary>
+        /// 执行任务。
+        /// </summary>
+        void ICommand.Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                throw new ArgumentException(
+                    $"不能向 ActionCommand 指定参数，因为这里指定的参数无法传递。如果希望传递参数，请使用 {nameof(ActionCommand)} 的泛型版本。",
+                    nameof(parameter));
+            }
+
+            Execute();
+        }
+
+        /// <summary>
+        /// 执行任务。
+        /// </summary>
+        public void Execute() => _action.Invoke();
+
+        /// <summary>
+        /// 判断命令何时可用。
+        /// </summary>
+        bool ICommand.CanExecute(object parameter) => true;
+
+        /// <summary>
+        /// 当命令的可执行性改变时发生。
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ src/Frameworks/Walterlv.Windows.Framework/Windows/Input/ActionCommand`1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Walterlv.Windows.Input
+{
+    /// <summary>
+    /// 表示一个必须提供参数才能执行的命令。
+    /// </summary>
+    public class ActionCommand<T> : ICommand
+    {
+        /// <summary>
+        /// 创建 <see cref="ActionCommand"/> 的新实例，当 <see cref="ICommand"/> 被执行时，将调用参数传入的动作。
+        /// </summary>
+        public ActionCommand(Action<T> action)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+
+        /// <summary>
+        /// 用于接受所提供的参数并执行的委托。
+        /// </summary>
+        private readonly Action<T> _action;
+
+        /// <summary>
+        /// 使用指定的参数执行此命令。
+        /// 框架中没有约定参数值是否允许为 null，这由参数定义时的泛型类型约定（C#8.0）或由命令的实现者约定。
+        /// </summary>
+        public void Execute(T t) => _action(t);
+
+        void ICommand.Execute(object parameter) => Execute((T)parameter);
+
+        bool ICommand.CanExecute(object parameter) => true;
+
+        /// <inheritdoc />
+        /// <summary>
+        /// 当命令的可执行性改变时发生。
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+    }
+}
