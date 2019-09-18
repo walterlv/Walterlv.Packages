2c8d3267321adc9fc8c41e37fe6036b2668f2d33
walterlv <xblero@gmail.com>
2019-09-18 19:22:27 +0800

    Add visual tree extensions.

---
+++ src/Utils/Walterlv.Windows/Media/VisualTreeExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+
+namespace Walterlv.Windows.Media
+{
+    public static class VisualTreeExtensions
+    {
+        public static Visual FindRoot(this Visual visual)
+        {
+            if (visual is null)
+            {
+                throw new ArgumentNullException(nameof(visual));
+            }
+
+            var current = visual;
+            var parent = VisualTreeHelper.GetParent(current) as Visual;
+            while (parent != null)
+            {
+                current = parent;
+                parent = VisualTreeHelper.GetParent(current) as Visual;
+            }
+            return current;
+        }
+
+        public static IEnumerable<T> FindDecendents<T>(this Visual visual,
+            Func<VisualTreeSearchConditions, VisualTreeSearchConditions> condition = null)
+            where T : Visual
+        {
+            if (visual is null)
+            {
+                throw new ArgumentNullException(nameof(visual));
+            }
+
+            foreach (var v in EnumerateDecendents(visual).OfType<T>())
+            {
+                var c = new VisualTreeSearchConditions(v);
+                c = condition(c);
+                if (c.CheckMatch())
+                {
+                    yield return v;
+                }
+            }
+        }
+
+        private static IEnumerable<Visual> EnumerateDecendents(Visual visual)
+        {
+            yield return visual;
+            var count = VisualTreeHelper.GetChildrenCount(visual);
+            for (var i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(visual, i);
+                if (child is Visual c)
+                {
+                    foreach (var grand in EnumerateDecendents(c))
+                    {
+                        yield return grand;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Utils/Walterlv.Windows/Media/VisualTreeSearchConditions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Walterlv.Windows.Media
+{
+    public class VisualTreeSearchConditions
+    {
+        private readonly Visual _self;
+        private Func<bool> _conditions;
+
+        internal VisualTreeSearchConditions(Visual self)
+        {
+            _self = self;
+        }
+
+        public VisualTreeSearchConditions ParentIs<T>(string name = null)
+            where T : Visual
+        {
+            var v = VisualTreeHelper.GetParent(_self);
+            _conditions += () => v is T && (v as FrameworkElement)?.Name?.Equals(name) is true;
+            return this;
+        }
+
+        internal bool CheckMatch()
+        {
+            return _conditions.GetInvocationList().Cast<Func<bool>>().All(x => x());
+        }
+    }
+}
