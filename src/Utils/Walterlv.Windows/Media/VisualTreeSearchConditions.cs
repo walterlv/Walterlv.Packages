2ce0e2b2905ff885afb4f9141169815474f1a51b
walterlv <xblero@gmail.com>
2019-11-20 20:22:07 +0800

    Add HasChild method.

--- src/Utils/Walterlv.Windows/Media/VisualTreeSearchConditions.cs
+++ src/Utils/Walterlv.Windows/Media/VisualTreeSearchConditions.cs
@@ -15,11 +15,37 @@ namespace Walterlv.Windows.Media
             _self = self;
         }
 
+        public VisualTreeSearchConditions NameIs(string name)
+        {
+            _conditions += () => (_self as FrameworkElement)?.Name?.Equals(name, StringComparison.Ordinal) is true;
+            return this;
+        }
+
         public VisualTreeSearchConditions ParentIs<T>(string name = null)
             where T : Visual
         {
             var v = VisualTreeHelper.GetParent(_self);
-            _conditions += () => v is T && (v as FrameworkElement)?.Name?.Equals(name) is true;
+            _conditions += () => v is T && (v as FrameworkElement)?.Name?.Equals(name, StringComparison.Ordinal) is true;
+            return this;
+        }
+
+        public VisualTreeSearchConditions HasChild<T>(string name = null)
+            where T : Visual
+        {
+            _conditions += () =>
+            {
+                var count = VisualTreeHelper.GetChildrenCount(_self);
+                for (var i = 0; i < count; i++)
+                {
+                    var v = VisualTreeHelper.GetChild(_self, i);
+                    var result = v is T && (v as FrameworkElement)?.Name?.Equals(name, StringComparison.Ordinal) is true;
+                    if (result)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
             return this;
         }
 
