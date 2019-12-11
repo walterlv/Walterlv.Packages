798b4e085708a31e14ca2e2be855f15207e4feeb
walterlv <xblero@gmail.com>
2019-12-11 21:06:30 +0800

    Add extensions.

---
+++ src/Utils/Walterlv.Collections/Concurrent/ReadonlyObservableBagExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Collections.Concurrent
+{
+    public static class ReadonlyObservableBagExtensions
+    {
+        public static ReadonlyObservableBag<T> Select<T>(this ObservableConcurrentBag<T> collection, Func<T, bool> predicate)
+        {
+            return new ReadonlyObservableBag<T>(collection, predicate);
+        }
+    }
+}
