c0cb963306b41ffb8ae47fb1873884dc0829d17c
walterlv <xblero@gmail.com>
2020-06-22 02:55:36 +0800

    增加窗口的拆离

--- src/Utils/Walterlv.Windows.Interop/Interop/WindowWrapper.cs
+++ src/Utils/Walterlv.Windows.Interop/Interop/WindowWrapper.cs
@@ -22,6 +22,8 @@ namespace Walterlv.Windows.Interop
             FocusVisualStyleProperty.OverrideMetadata(typeof(WindowWrapper), new FrameworkPropertyMetadata(null));
         }
 
+        private readonly WindowStyles _originalChildWindowStyles;
+
         /// <summary>
         /// 创建包装句柄为 <paramref name="childHandle"/> 窗口的 <see cref="WindowWrapper"/> 的实例。
         /// </summary>
@@ -37,8 +39,8 @@ namespace Walterlv.Windows.Interop
             PreviewKeyDown += OnKeyDown;
 
             // 设置窗口样式为子窗口。这里的样式值与 HwndHost/HwndSource 成对时设置的值一模一样。
-            User32.SetWindowLong(childHandle,
-                GetWindowLongIndexes.GWL_STYLE,
+            _originalChildWindowStyles = (WindowStyles)User32.GetWindowLong(Handle, GetWindowLongIndexes.GWL_STYLE);
+            User32.SetWindowLong(Handle, GetWindowLongIndexes.GWL_STYLE,
                 (IntPtr)(WindowStyles.WS_CHILDWINDOW | WindowStyles.WS_VISIBLE | WindowStyles.WS_CLIPCHILDREN));
         }
 
@@ -47,6 +49,42 @@ namespace Walterlv.Windows.Interop
         /// </summary>
         public IntPtr Handle { get; }
 
+        /// <summary>
+        /// 获取或设置当前窗口是否已被捕获和自动管理中。
+        /// 默认为 true，即窗口不当作控件自动管理；如果设置为 false，则窗口与此控件断开联系。
+        /// </summary>
+        public bool IsCaptured
+        {
+            get => (bool)GetValue(IsCapturedProperty);
+            set => SetValue(IsCapturedProperty, value);
+        }
+
+        /// <summary>
+        /// 标识 <see cref="IsCaptured"/> 的附加属性。
+        /// </summary>
+        public static readonly DependencyProperty IsCapturedProperty = DependencyProperty.Register(
+            nameof(IsCaptured), typeof(bool), typeof(WindowWrapper),
+            new PropertyMetadata(true, (sender, args) => ((WindowWrapper)sender).OnIsCapturedChanged((bool)args.OldValue, (bool)args.NewValue)));
+
+        private async void OnIsCapturedChanged(bool oldValue, bool newValue)
+        {
+            if (newValue)
+            {
+                if (IsVisible)
+                {
+                    await ShowChildAsync().ConfigureAwait(true);
+                    await ArrangeChildAsync().ConfigureAwait(true);
+                    IsChildStyle = true;
+                }
+            }
+            else
+            {
+                await ShowChildAsync().ConfigureAwait(true);
+                await ArrangeChildAsync().ConfigureAwait(true);
+                IsChildStyle = false;
+            }
+        }
+
         private async void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             var parent = Window.GetWindow(this);
@@ -54,17 +92,30 @@ namespace Walterlv.Windows.Interop
             {
                 LayoutUpdated -= OnLayoutUpdated;
                 LayoutUpdated += OnLayoutUpdated;
+                if (!IsCaptured)
+                {
+                    return;
+                }
                 await ShowChildAsync().ConfigureAwait(false);
             }
             else
             {
                 LayoutUpdated -= OnLayoutUpdated;
+                if (!IsCaptured)
+                {
+                    return;
+                }
                 await HideChildAsync().ConfigureAwait(false);
             }
         }
 
         private void OnGotFocus(object sender, RoutedEventArgs e)
         {
+            if (!IsCaptured)
+            {
+                return;
+            }
+
             // 设置子窗口获取焦点。
             // 这样，Tab 键的切换以及快捷键将仅在 Shell 端生效。
             User32.SetFocus(Handle);
@@ -72,6 +123,11 @@ namespace Walterlv.Windows.Interop
 
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
+            if (!IsCaptured)
+            {
+                return;
+            }
+
             // 当此控件获取焦点的时候，将吞掉全部的键盘事件：
             // 1. 将所有事件转发到子窗口；
             // 2. 避免按键被父窗口响应（此控件获取焦点可理解为已经切换到子窗口了，不能让父窗口处理事件）；
@@ -88,10 +144,37 @@ namespace Walterlv.Windows.Interop
 
         private void OnLayoutUpdated(object? sender, EventArgs e)
         {
+            if (!IsCaptured)
+            {
+                return;
+            }
+
             // 最终决定在 LayoutUpdated 事件里面更新子窗口的位置和尺寸，因为：
             //  1. Arrange 仅在大小改变的时候才会触发，这使得如果外面更新了 Margin 等导致位置改变大小不变时（例如窗口最大化），窗口位置不会刷新。
             //  2. 使用 Loaded 优先级延迟布局可以解决以上问题，但会导致布局更新不及时，部分依赖于布局的代码（例如拖拽调整窗口大小）会计算错误。
-            _ = ArrangeChildAsync(new Size(ActualWidth, ActualHeight));
+            _ = ArrangeChildAsync();
+        }
+
+        /// <summary>
+        /// 获取或设置子窗口当前是否是子窗口样式。
+        /// </summary>
+        private bool IsChildStyle
+        {
+            get
+            {
+                var style = (WindowStyles)User32.GetWindowLong(Handle, GetWindowLongIndexes.GWL_STYLE);
+                return (int)(style & WindowStyles.WS_CHILDWINDOW) != 0;
+            }
+            set
+            {
+                var expectedStyle = value
+                    ? WindowStyles.WS_CHILDWINDOW | WindowStyles.WS_CLIPCHILDREN
+                    : _originalChildWindowStyles;
+                expectedStyle = IsVisible
+                    ? expectedStyle | WindowStyles.WS_VISIBLE
+                    : expectedStyle & ~WindowStyles.WS_VISIBLE;
+                User32.SetWindowLong(Handle, GetWindowLongIndexes.GWL_STYLE, (IntPtr)expectedStyle);
+            }
         }
 
         /// <summary>
@@ -104,11 +187,18 @@ namespace Walterlv.Windows.Interop
                 ? parentSource.Handle
                 : IntPtr.Zero;
 
-            // 连接子窗口。
-            // 注意连接子窗口后，窗口的消息循环会强制同步，这可能降低 UI 的响应性能。详情请参考：
-            // https://blog.walterlv.com/post/all-processes-freezes-if-their-windows-are-connected-via-setparent.html
-            User32.SetParent(Handle, hwndParent);
-            Debug.WriteLine("[Window] 嵌入");
+            if (IsCaptured)
+            {
+                // 连接子窗口。
+                // 注意连接子窗口后，窗口的消息循环会强制同步，这可能降低 UI 的响应性能。详情请参考：
+                // https://blog.walterlv.com/post/all-processes-freezes-if-their-windows-are-connected-via-setparent.html
+                User32.SetParent(Handle, hwndParent);
+                Debug.WriteLine("[Window] 嵌入");
+            }
+            else
+            {
+                User32.SetParent(Handle, IntPtr.Zero);
+            }
 
             // 显示子窗口。
             // 注意调用顺序：先嵌入子窗口，这可以避免任务栏中出现窗口图标。
@@ -142,17 +232,24 @@ namespace Walterlv.Windows.Interop
         /// 布局子窗口。
         /// </summary>
         /// <param name="size">设定子窗口的显示尺寸。</param>
-        private async Task ArrangeChildAsync(Size size)
+        private async Task ArrangeChildAsync(Size? size = default)
         {
-            var presentationSource = PresentationSource.FromVisual(this);
-            if (presentationSource is null)
+            if (size == null)
+            {
+                size = !IsCaptured && User32.GetWindowRect(Handle, out var oldRect)
+                    ? (Size?)new Size(oldRect.right - oldRect.left, oldRect.bottom - oldRect.top)
+                    : (Size?)new Size(ActualWidth, ActualHeight);
+            }
+            var (width, height) = (size.Value.Width, size.Value.Height);
+            var hwndSource = PresentationSource.FromVisual(this) as HwndSource;
+            if (hwndSource is null)
             {
                 // 因为此方法可能被延迟执行，所以可能此元素已经不在可视化树了，这时会进入此分支。
                 return;
             }
 
             // 获取子窗口相对于父窗口的相对坐标。
-            var transform = TransformToAncestor(presentationSource.RootVisual);
+            var transform = TransformToAncestor(hwndSource.RootVisual);
             var scalingFactor = this.GetScalingRatioToDevice();
             var offset = transform.Transform(default);
 
@@ -162,8 +259,16 @@ namespace Walterlv.Windows.Interop
             // 移动子窗口到合适的布局位置。
             var x = (int)(offset.X * scalingFactor.Width);
             var y = (int)(offset.Y * scalingFactor.Height);
-            var w = (int)(size.Width * scalingFactor.Width);
-            var h = (int)(size.Height * scalingFactor.Height);
+            var w = (int)(width * scalingFactor.Width);
+            var h = (int)(height * scalingFactor.Height);
+
+            if (!IsCaptured && User32.GetWindowRect(hwndSource.Handle, out var ownerRect))
+            {
+                // 如果窗口未被捕获，则还需叠加主窗口坐标。
+                x += ownerRect.left;
+                y += ownerRect.top;
+            }
+
             User32.MoveWindow(Handle, x, y, w, h, true);
         }
 
