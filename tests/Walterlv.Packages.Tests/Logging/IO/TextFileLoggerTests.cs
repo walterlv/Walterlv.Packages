15043c54973b13b4a26e112bd853194fb31f12fc
walterlv <xblero@gmail.com>
2020-06-30 19:33:51 +0800

    加上自动清除文件的单元测试

--- tests/Walterlv.Packages.Tests/Logging/IO/TextFileLoggerTests.cs
+++ tests/Walterlv.Packages.Tests/Logging/IO/TextFileLoggerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MSTest.Extensions.Contracts;
 using Walterlv.Logging.IO;
@@ -51,6 +52,40 @@ namespace Walterlv.Tests.Logging.IO
 
                 var newLines = File.ReadAllLines(testFile);
                 Assert.AreEqual(4, newLines.Length);
+            });
+        }
+
+        [ContractTestCase]
+        public void DeleteFileWhenInitialize()
+        {
+            "初始化时，超过大小的文件内容会清空。".Test(() =>
+            {
+                const string testFile = "test21.md";
+                File.WriteAllText(testFile, "XXXXXXXX\n");
+
+                var aLogger = new TextFileLogger(new FileInfo(testFile))
+                    .WithMaxFileSize(4);
+                aLogger.Message("YY");
+                aLogger.Close();
+
+                var lines = File.ReadAllLines(testFile);
+                Assert.AreEqual(1, lines.Length);
+                Assert.IsTrue(lines[0].Contains("YY", StringComparison.Ordinal));
+            });
+
+            "初始化时，超过行数的文件前面行会删除。".Test(() =>
+            {
+                const string testFile = "test22.md";
+                File.WriteAllText(testFile, "XXXXXXXX\n\nYYYYYYYY\nZZZZZZZZ\n");
+
+                var aLogger = new TextFileLogger(new FileInfo(testFile))
+                    .WithMaxLineCount(3, 1);
+                aLogger.Message("WW");
+                aLogger.Close();
+
+                var lines = File.ReadAllLines(testFile);
+                Assert.AreEqual(2, lines.Length);
+                Assert.IsTrue(lines[0].Contains("ZZZZZZZZ", StringComparison.Ordinal));
             });
         }
     }
