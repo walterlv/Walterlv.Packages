6ba42f8f067b0de7092136d2cf6d37d77ac14461
walterlv <xblero@gmail.com>
2020-07-07 16:41:42 +0800

    完美处理 NullableAttributes 的多框架兼容
    # Conflicts:
    #	Walterlv.Packages.sln

--- src/Diagnostics/Walterlv.NullableAttributes.Source/CodeAnalysis.cs
+++ src/Diagnostics/Walterlv.NullableAttributes.Source/CodeAnalysis.cs
@@ -1,50 +1,37 @@
-﻿#if NETCOREAPP3_0
+﻿#if NETCOREAPP3_0 || NETCOREAPP3_1 || NETCOREAPP5_1 || NET5_0 || NET6_0
 // 新框架都包含 Nullable Attributes。
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-
-[assembly:TypeForwardedTo(typeof(AllowNullAttribute))]
-[assembly:TypeForwardedTo(typeof(DisallowNullAttribute))]
-[assembly:TypeForwardedTo(typeof(MaybeNullAttribute))]
-[assembly:TypeForwardedTo(typeof(NotNullAttribute))]
-[assembly:TypeForwardedTo(typeof(MaybeNullWhenAttribute))]
-[assembly:TypeForwardedTo(typeof(NotNullWhenAttribute))]
-[assembly:TypeForwardedTo(typeof(NotNullIfNotNullAttribute))]
-[assembly:TypeForwardedTo(typeof(DoesNotReturnAttribute))]
-[assembly:TypeForwardedTo(typeof(DoesNotReturnIfAttribute))]
 #else
-// 旧框架需要包含 Nullable Attributes。
 namespace System.Diagnostics.CodeAnalysis
 {
     /// <summary>
     /// 标记一个不可空的输入实际上是可以传入 null 的。
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
-    public sealed class AllowNullAttribute : Attribute { }
+    internal sealed class AllowNullAttribute : Attribute { }
 
     /// <summary>
     /// 标记一个可空的输入实际上不应该传入 null。
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
-    public sealed class DisallowNullAttribute : Attribute { }
+    internal sealed class DisallowNullAttribute : Attribute { }
 
     /// <summary>
     /// 标记一个非空的返回值实际上可能会返回 null，返回值包括输出参数。
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-    public sealed class MaybeNullAttribute : Attribute { }
+    internal sealed class MaybeNullAttribute : Attribute { }
 
     /// <summary>
     /// 标记一个可空的返回值实际上是不可能返回 null 的，返回值包括输出参数。
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-    public sealed class NotNullAttribute : Attribute { }
+    internal sealed class NotNullAttribute : Attribute { }
 
     /// <summary>
     /// 当返回指定的 true/false 时某个输出参数才可能为 null，而返回相反的值时那个输出参数则不可为 null。
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    public sealed class MaybeNullWhenAttribute : Attribute 
+    internal sealed class MaybeNullWhenAttribute : Attribute 
     {
         /// <summary>
         /// 使用 true 或者 false 决定输出参数是否可能为 null。
@@ -62,7 +49,7 @@ namespace System.Diagnostics.CodeAnalysis
     /// 当返回指定的 true/false 时，某个输出参数不可为 null，而返回相反的值时那个输出参数则可能为 null。
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    public sealed class NotNullWhenAttribute : Attribute
+    internal sealed class NotNullWhenAttribute : Attribute
     {
         /// <summary>
         /// 使用 true 或者 false 决定输出参数是否不可为 null。
@@ -80,7 +67,7 @@ namespace System.Diagnostics.CodeAnalysis
     /// 指定的参数传入 null 时才可能返回 null，指定的参数传入非 null 时就不可能返回 null。
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
-    public sealed class NotNullIfNotNullAttribute : Attribute
+    internal sealed class NotNullIfNotNullAttribute : Attribute
     {
         /// <summary>
         /// 使用一个参数名称决定返回值是否可能为 null。
@@ -100,14 +87,14 @@ namespace System.Diagnostics.CodeAnalysis
     /// 指定一个方法是不可能返回的。
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-    public sealed class DoesNotReturnAttribute : Attribute
+    internal sealed class DoesNotReturnAttribute : Attribute
     { }
 
     /// <summary>
     /// 在方法的输入参数上指定一个条件，当这个参数传入了指定的 true/false 时方法不可能返回。
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    public sealed class DoesNotReturnIfAttribute : Attribute
+    internal sealed class DoesNotReturnIfAttribute : Attribute
     {
         /// <summary>
         /// 使用 true/false 决定方法是否可能返回。
