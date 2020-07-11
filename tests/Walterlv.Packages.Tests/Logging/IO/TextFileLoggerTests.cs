81c71fa41bc18b2b8433d7eed5a229a8d83dd428
walterlv <xblero@gmail.com>
2020-07-11 11:54:57 +0800

    增加日志的基础单元测试

--- tests/Walterlv.Packages.Tests/Logging/IO/TextFileLoggerTests.cs
+++ tests/Walterlv.Packages.Tests/Logging/IO/TextFileLoggerTests.cs
@@ -10,37 +10,63 @@ namespace Walterlv.Tests.Logging.IO
     public class TextFileLoggerTests
     {
         [ContractTestCase]
+        public void WriteInfoErrorLog()
+        {
+            "写入 Info 级别的日志，发现日志已被写入。".Test(() =>
+            {
+                var (logger, infoFile, errorFile) = PrepareFileNotExistLogger("info01.md", "error01.md");
+
+                logger.Message("aaaa");
+                logger.Message("bbbb");
+                logger.Close();
+
+                Assert.AreEqual(2, File.ReadAllLines(infoFile.FullName).Length);
+                Assert.IsFalse(File.Exists(errorFile.FullName));
+            });
+
+            "写入 Error 级别的日志，发现日志已被写入。".Test(() =>
+            {
+                var (logger, infoFile, errorFile) = PrepareFileNotExistLogger("info02.md", "error02.md");
+
+                logger.Error("aaaa");
+                logger.Error("bbbb");
+                logger.Close();
+
+                Assert.AreEqual(2, File.ReadAllLines(infoFile.FullName).Length);
+                Assert.AreEqual(2, File.ReadAllLines(errorFile.FullName).Length);
+            });
+
+            "写入 Info 和 Error 级别的日志，发现日志已被写入。".Test(() =>
+            {
+                var (logger, infoFile, errorFile) = PrepareFileNotExistLogger("info03.md", "error03.md");
+
+                logger.Message("aaaa");
+                logger.Error("bbbb");
+                logger.Close();
+
+                Assert.AreEqual(2, File.ReadAllLines(infoFile.FullName).Length);
+                Assert.AreEqual(1, File.ReadAllLines(errorFile.FullName).Length);
+            });
+        }
+
+        [ContractTestCase]
         public void ReadWriteShare()
         {
             "关闭日志写入后，文件确保全部写入完成。".Test(() =>
             {
-                const string testFile = "test1.md";
-                if (File.Exists(testFile))
-                {
-                    File.Delete(testFile);
-                }
+                var (logger, testFile) = PrepareFileNotExistLogger("test11.md");
 
-                var aLogger = new TextFileLogger(new FileInfo(testFile));
+                logger.Message("aaaa");
+                logger.Message("bbbb");
+                logger.Close();
 
-                aLogger.Message("aaaa");
-                aLogger.Message("bbbb");
-
-                aLogger.Close();
-
-                var newLines = File.ReadAllLines(testFile);
-                Assert.AreEqual(2, newLines.Length);
+                Assert.AreEqual(2, File.ReadAllLines(testFile.FullName).Length);
             });
 
             "测试多个日志类读写同一个文件，所有内容都没丢。".Test(() =>
             {
-                const string testFile = "test2.md";
-                if (File.Exists(testFile))
-                {
-                    File.Delete(testFile);
-                }
-
-                var aLogger = new TextFileLogger(new FileInfo(testFile));
-                var bLogger = new TextFileLogger(new FileInfo(testFile));
+                var (aLogger, testFile) = PrepareFileNotExistLogger("test12.md");
+                var (bLogger, _) = PrepareFileNotExistLogger("test12.md");
 
                 aLogger.Message("aaaa");
                 bLogger.Message("bbbb");
@@ -50,8 +76,7 @@ namespace Walterlv.Tests.Logging.IO
                 aLogger.Close();
                 bLogger.Close();
 
-                var newLines = File.ReadAllLines(testFile);
-                Assert.AreEqual(4, newLines.Length);
+                Assert.AreEqual(4, File.ReadAllLines(testFile.FullName).Length);
             });
         }
 
@@ -60,15 +85,13 @@ namespace Walterlv.Tests.Logging.IO
         {
             "使用 With 初始化时，不能要求文件存在。".Test((Func<TextFileLogger, TextFileLogger> extraBuilder) =>
             {
-                const string testFile = "test21.md";
-                if (File.Exists(testFile))
-                {
-                    File.Delete(testFile);
-                }
+                var testFile = PrepareNotExistFile("test21.md");
 
-                var aLogger = extraBuilder(new TextFileLogger(new FileInfo(testFile)));
+                var aLogger = extraBuilder(new TextFileLogger(new FileInfo(testFile.FullName)));
                 aLogger.Message("YY");
                 aLogger.Close();
+
+                // 无异常。
             }).WithArguments(
                 x => x.WithMaxFileSize(100),
                 x => x.WithMaxLineCount(100),
@@ -105,6 +128,28 @@ namespace Walterlv.Tests.Logging.IO
                 Assert.AreEqual(2, lines.Length);
                 Assert.IsTrue(lines[0].Contains("ZZZZZZZZ"));
             });
+        }
+
+        private static FileInfo PrepareNotExistFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            return new FileInfo(fileName);
+        }
+
+        private static (TextFileLogger logger, FileInfo loggerFile) PrepareFileNotExistLogger(string fileName)
+        {
+            var file = PrepareNotExistFile(fileName);
+            return (new TextFileLogger(file), file);
+        }
+
+        private static (TextFileLogger logger, FileInfo infoFile, FileInfo errorFile) PrepareFileNotExistLogger(string infoFileName, string errorFileName)
+        {
+            var infoFile = PrepareNotExistFile(infoFileName);
+            var errorFile = PrepareNotExistFile(errorFileName);
+            return (new TextFileLogger(infoFile, errorFile), infoFile, errorFile);
         }
     }
 }
