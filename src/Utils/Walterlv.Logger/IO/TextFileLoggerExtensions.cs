7e02579adb96f2310c01e27b9a52e3fac749966e
walterlv <xblero@gmail.com>
2020-08-21 14:23:12 +0800

    初始化 Logger 的时候，Builder 模式不改变类型。

--- src/Utils/Walterlv.Logger/IO/TextFileLoggerExtensions.cs
+++ src/Utils/Walterlv.Logger/IO/TextFileLoggerExtensions.cs
@@ -15,7 +15,8 @@ namespace Walterlv.Logging.IO
         /// <param name="logger">日志实例。</param>
         /// <param name="maxFileSize">大于此大小（字节）时清空日志。</param>
         /// <returns>构造器模式。</returns>
-        public static TextFileLogger WithMaxFileSize(this TextFileLogger logger, long maxFileSize)
+        public static TTextFileLogger WithMaxFileSize<TTextFileLogger>(this TTextFileLogger logger, long maxFileSize)
+            where TTextFileLogger : TextFileLogger
         {
             if (maxFileSize <= 0)
             {
@@ -41,7 +42,8 @@ namespace Walterlv.Logging.IO
         /// <param name="maxLineCount">大于此行数时清空日志的前面行。</param>
         /// <param name="newLineCountAfterLimitReached">清空后应该保留行数。默认为完全不保留。</param>
         /// <returns>构造器模式。</returns>
-        public static TextFileLogger WithMaxLineCount(this TextFileLogger logger, int maxLineCount, int newLineCountAfterLimitReached = 0)
+        public static TTextFileLogger WithMaxLineCount<TTextFileLogger>(this TTextFileLogger logger, int maxLineCount, int newLineCountAfterLimitReached = 0)
+            where TTextFileLogger : TextFileLogger
         {
             if (maxLineCount <= 0)
             {
@@ -86,7 +88,8 @@ namespace Walterlv.Logging.IO
         /// <param name="logger">日志实例。</param>
         /// <param name="override">如果需要覆盖，请设置为 true。</param>
         /// <returns>构造器模式。</returns>
-        public static TextFileLogger WithWholeFileOverride(this TextFileLogger logger, bool @override = true)
+        public static TTextFileLogger WithWholeFileOverride<TTextFileLogger>(this TTextFileLogger logger, bool @override = true)
+            where TTextFileLogger : TextFileLogger
         {
             if (@override)
             {
@@ -103,7 +106,8 @@ namespace Walterlv.Logging.IO
         /// <param name="overrideForInfo">如果你希望首次写入信息日志时覆盖原来日志的整个文件，则设为 true；如果希望保留之前的日志而追加，则设为 false。</param>
         /// <param name="overrideForError">如果你希望首次写入错误日志时覆盖原来日志的整个文件，则设为 true；如果希望保留之前的日志而追加，则设为 false。</param>
         /// <returns>构造器模式。</returns>
-        public static TextFileLogger WithWholeFileOverride(this TextFileLogger logger, bool overrideForInfo, bool overrideForError)
+        public static TTextFileLogger WithWholeFileOverride<TTextFileLogger>(this TTextFileLogger logger, bool overrideForInfo, bool overrideForError)
+            where TTextFileLogger : TextFileLogger
         {
             if (overrideForInfo || overrideForError)
             {
