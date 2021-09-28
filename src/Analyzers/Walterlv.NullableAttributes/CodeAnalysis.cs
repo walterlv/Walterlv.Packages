14ad919679a555d52e4b315ed3cab5760def696c
walterlv <xblero@gmail.com>
2021-09-28 17:47:22 +0800

    加入 .NET 5.0 新增的 Nullable Attributes.

--- src/Analyzers/Walterlv.NullableAttributes/CodeAnalysis.cs
+++ src/Analyzers/Walterlv.NullableAttributes/CodeAnalysis.cs
@@ -1,5 +1,5 @@
-﻿#if NETCOREAPP3_0
-// 新框架都包含 Nullable Attributes。
+﻿#if NETCOREAPP3_0 || NETCOREAPP3_1 || NET5_0_OR_GREATER
+// 新框架都包含基本的 Nullable Attributes。
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
@@ -44,7 +44,7 @@ namespace System.Diagnostics.CodeAnalysis
     /// 当返回指定的 true/false 时某个输出参数才可能为 null，而返回相反的值时那个输出参数则不可为 null。
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    public sealed class MaybeNullWhenAttribute : Attribute 
+    public sealed class MaybeNullWhenAttribute : Attribute
     {
         /// <summary>
         /// 使用 true 或者 false 决定输出参数是否可能为 null。
@@ -67,7 +67,9 @@ namespace System.Diagnostics.CodeAnalysis
         /// <summary>
         /// 使用 true 或者 false 决定输出参数是否不可为 null。
         /// </summary>
-        /// <param name="returnValue">如果方法返回值等于这个值，那么输出参数则不可为 null，否则输出参数是可能为 null 的。</param>
+        /// <param name="returnValue">
+        /// 如果方法或属性的返回值等于这个值，那么输出参数则不可为 null，否则输出参数是可能为 null 的。
+        /// </param>
         public NotNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
 
         /// <summary>
@@ -100,8 +102,7 @@ namespace System.Diagnostics.CodeAnalysis
     /// 指定一个方法是不可能返回的。
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-    public sealed class DoesNotReturnAttribute : Attribute
-    { }
+    public sealed class DoesNotReturnAttribute : Attribute { }
 
     /// <summary>
     /// 在方法的输入参数上指定一个条件，当这个参数传入了指定的 true/false 时方法不可能返回。
@@ -121,6 +122,91 @@ namespace System.Diagnostics.CodeAnalysis
         /// 获取决定方法是否可返回的那个参数的值。
         /// </summary>
         public bool ParameterValue { get; }
+    }
+}
+#endif
+
+#if NET5_0_OR_GREATER
+// .NET 5.0 开始已包含更多 Nullable Attributes。
+[assembly: TypeForwardedTo(typeof(MemberNotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(MemberNotNullWhenAttribute))]
+#else
+// 旧框架需要包含 Nullable Attributes。
+namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>
+    /// 调用了此方法后，即可保证列表中所列出的字段和属性成员将不会为 null。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public sealed class MemberNotNullAttribute : Attribute
+    {
+        /// <summary>
+        /// 指定调用了此方法后，所列出的字段和属性成员将不会为 null。
+        /// </summary>
+        /// <param name="member">
+        /// 将保证不会为 null 的字段或属性名称。
+        /// </param>
+        public MemberNotNullAttribute(string member) => Members = new[] { member };
+
+        /// <summary>
+        /// 指定调用了此方法后，所列出的字段和属性成员将不会为 null。
+        /// </summary>
+        /// <param name="members">
+        /// 将保证不会为 null 的字段或属性名称列表。
+        /// </param>
+        public MemberNotNullAttribute(params string[] members) => Members = members;
+
+        /// <summary>
+        /// 调用了此方法后保证不会为 null 的字段或属性名称列表。
+        /// </summary>
+        public string[] Members { get; }
+    }
+
+    /// <summary>
+    /// 当返回指定的 true/false 时，即可保证列表中所列出的字段和属性成员将不会为 null。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public sealed class MemberNotNullWhenAttribute : Attribute
+    {
+        /// <summary>
+        /// 使用 true 或者 false 决定是否所列出的字段和属性成员将不会为 null。
+        /// </summary>
+        /// <param name="returnValue">
+        /// 如果方法或属性的返回值等于这个值，那么所列出的字段和属性成员将不会为 null。
+        /// </param>
+        /// <param name="member">
+        /// 将保证不会为 null 的字段或属性名称列表。
+        /// </param>
+        public MemberNotNullWhenAttribute(bool returnValue, string member)
+        {
+            ReturnValue = returnValue;
+            Members = new[] { member };
+        }
+
+        /// <summary>
+        /// 使用 true 或者 false 决定是否所列出的字段和属性成员将不会为 null。
+        /// </summary>
+        /// <param name="returnValue">
+        /// 如果方法或属性的返回值等于这个值，那么所列出的字段和属性成员将不会为 null。
+        /// </param>
+        /// <param name="members">
+        /// 将保证不会为 null 的字段或属性名称列表。
+        /// </param>
+        public MemberNotNullWhenAttribute(bool returnValue, params string[] members)
+        {
+            ReturnValue = returnValue;
+            Members = members;
+        }
+
+        /// <summary>
+        /// 获取返回值决定是否不可为空的那个判断值。
+        /// </summary>
+        public bool ReturnValue { get; }
+
+        /// <summary>
+        /// 调用了此方法后保证不会为 null 的字段或属性名称列表。
+        /// </summary>
+        public string[] Members { get; }
     }
 }
 #endif
