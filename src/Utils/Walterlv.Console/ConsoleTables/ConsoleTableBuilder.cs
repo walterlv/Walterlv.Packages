abd4f3430f0d893c047fe4029575acca047193d9
walterlv <xblero@gmail.com>
2023-08-03 11:23:34 +0800

    Add new package Walterlv.Console

---
+++ src/Utils/Walterlv.Console/ConsoleTables/ConsoleTableBuilder.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Walterlv.ForegroundWindowMonitor;
+
+/// <summary>
+/// 表示一个控制台表格的列定义。
+/// </summary>
+/// <typeparam name="T">表格中每一行的数据类型。</typeparam>
+public readonly record struct ConsoleTableColumnDefinition<T> where T : notnull
+{
+    public ConsoleTableColumnDefinition(string text, Func<T, string> columnValueFormatter)
+    {
+        Width = text.Length;
+        WidthPercent = 0;
+        Text = text ?? throw new ArgumentNullException(nameof(text));
+        ColumnValueFormatter = columnValueFormatter;
+    }
+
+    public ConsoleTableColumnDefinition(int width, string text, Func<T, string> columnValueFormatter)
+    {
+        if (width <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(width), width, "Width must be greater than 0.");
+        }
+
+        Width = width;
+        WidthPercent = 0;
+        Text = text ?? throw new ArgumentNullException(nameof(text));
+        ColumnValueFormatter = columnValueFormatter;
+    }
+
+    public ConsoleTableColumnDefinition(double widthPercent, string text, Func<T, string> columnValueFormatter)
+    {
+        if (widthPercent <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(widthPercent), widthPercent, "Width percent must be greater than 0.");
+        }
+
+        Width = 0;
+        WidthPercent = widthPercent;
+        Text = text ?? throw new ArgumentNullException(nameof(text));
+        ColumnValueFormatter = columnValueFormatter;
+    }
+
+    /// <summary>
+    /// 获取列的字符显示宽度。
+    /// </summary>
+    public int Width { get; }
+
+    /// <summary>
+    /// 获取列的字符显示宽度百分比。
+    /// </summary>
+    /// <remarks>
+    /// 指定了 <see cref="Width"/> 的列不参与计算百分比，其他列按百分比分剩余宽度。
+    /// <para/>
+    /// 所有列宽度百分比的总和允许大于 100%。当大于时，会压缩每一列按百分比计算的宽度。
+    /// </remarks>
+    public double WidthPercent { get; }
+
+    /// <summary>
+    /// 获取列的标题。
+    /// </summary>
+    public string Text { get; }
+
+    /// <summary>
+    /// 获取列的值格式化器。
+    /// </summary>
+    public Func<T, string> ColumnValueFormatter { get; }
+
+    public static implicit operator ConsoleTableColumnDefinition<T>(string headerText)
+    {
+        return new ConsoleTableColumnDefinition<T>(headerText, v => v.ToString()!);
+    }
+
+    public static implicit operator ConsoleTableColumnDefinition<T>((int Width, string Text) header)
+    {
+        return new ConsoleTableColumnDefinition<T>(header.Width, header.Text, v => v.ToString()!);
+    }
+
+    public static implicit operator ConsoleTableColumnDefinition<T>((int Width, string Text, Func<T, string> ColumnValueFormatter) header)
+    {
+        return new ConsoleTableColumnDefinition<T>(header.Width, header.Text, header.ColumnValueFormatter);
+    }
+
+    public static implicit operator ConsoleTableColumnDefinition<T>((double WidthPercent, string Text) header)
+    {
+        return new ConsoleTableColumnDefinition<T>(header.WidthPercent, header.Text, v => v.ToString()!);
+    }
+
+    public static implicit operator ConsoleTableColumnDefinition<T>((double WidthPercent, string Text, Func<T, string> ColumnValueFormatter) header)
+    {
+        return new ConsoleTableColumnDefinition<T>(header.WidthPercent, header.Text, header.ColumnValueFormatter);
+    }
+}
---
+++ src/Utils/Walterlv.Console/Utils/ConsoleStringExtensions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Walterlv.ConsoleExtensions.Utils;
+/// <summary>
+/// 提供 <see cref="string"/> 的控制台相关扩展方法。
+/// </summary>
+public static class ConsoleStringExtensions
+{
+    /// <summary>
+    /// 获取字符串在控制台中的长度。
+    /// </summary>
+    /// <param name="str">要获取长度的字符串。</param>
+    /// <returns>字符串在控制台中的长度。</returns>
+    public static int GetConsoleLength(this string str)
+    {
+        if (str is null)
+        {
+            throw new ArgumentNullException(nameof(str));
+        }
+
+        var totalLength = 0;
+        for (var i = 0; i < str.Length; i++)
+        {
+            totalLength += str[i].GetConsoleLength();
+        }
+        return totalLength;
+    }
+
+    /// <summary>
+    /// 在控制台中，将字符串填充到指定长度，如果超过则截断，如果不足则填充。
+    /// </summary>
+    /// <param name="str">要填充或截断的字符串。</param>
+    /// <param name="totalWidth">要填充到的长度。</param>
+    /// <param name="paddingChar">填充的字符。</param>
+    /// <param name="trimEndIfExceeds">如果超过长度，是否截断。</param>
+    /// <returns>填充或截断后的字符串。</returns>
+    public static string ConsolePadRight(this string str, int totalWidth, char paddingChar, bool trimEndIfExceeds)
+    {
+        if (str is null)
+        {
+            throw new ArgumentNullException(nameof(str));
+        }
+
+        if (totalWidth <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(totalWidth));
+        }
+
+        var consoleLength = str.GetConsoleLength();
+        if (consoleLength > totalWidth)
+        {
+            if (trimEndIfExceeds)
+            {
+                var sb = new StringBuilder(str);
+                for (var i = str.Length - 1; i >= 0; i--)
+                {
+                    consoleLength -= sb[i].GetConsoleLength();
+                    sb.Length = i;
+                    if (consoleLength <= totalWidth)
+                    {
+                        var paddingCount = totalWidth - consoleLength;
+                        sb.Append(paddingChar, paddingCount);
+                        return sb.ToString();
+                    }
+                }
+            }
+        }
+        else if (consoleLength < totalWidth)
+        {
+            var sb = new StringBuilder(str);
+            var paddingCount = totalWidth - consoleLength;
+            sb.Append(paddingChar, paddingCount);
+            return sb.ToString();
+        }
+
+        return str;
+    }
+
+    /// <summary>
+    /// 获取字符在控制台中的长度。
+    /// </summary>
+    /// <param name="c">要获取长度的字符。</param>
+    /// <returns>字符在控制台中的长度。</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int GetConsoleLength(this char c)
+    {
+        if (CharUnicodeInfo.GetUnicodeCategory(c) is UnicodeCategory.OtherLetter)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
